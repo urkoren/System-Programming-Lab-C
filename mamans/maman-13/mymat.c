fd6c05fcb9f8cdbd083ca601edf53e20155ce57d
zxzxxxx <ork14790@gmail.com>
2024-01-15 01:07:18 +0200

    Mam13

--- mamans/maman-13/mymat.c
+++ mamans/maman-13/mymat.c
@@ -1,34 +1,49 @@
-#include <stdio.h>
-#include <string.h>
+#include "mymat.h"
 
-#define LEN_OF_MAT 4
-
-typedef struct {
-    float values[LEN_OF_MAT][LEN_OF_MAT];
-} MAT;
-
-typedef struct {
-    MAT *MAT_A;
-}MATRICES;
-
-void reset_mat(MAT *mat) {
-    memset(mat->values, 0, sizeof(mat->values));
+void 
+reset_mat(MAT mat) {
+    memset(mat.data, 0, sizeof(mat.data));
 }
 
-void print_mat(const MATRICES matrices) {
-    MAT mat;
+void 
+pprint_mat(const char *matrix_name, const MAT matrix_data) 
+{
+    int i, j;
+
+    fprintf(stdout, "%s = [", matrix_name);
+    for (i = 0; i < LEN_OF_MAT; i++) {
+        fprintf(stdout, "|\n");
+        for (j = 0; j < LEN_OF_MAT; j++) {
+            fprintf(stdout, "%*s\n", 4, " ");
+            fprintf(stdout, "%.2f", matrix_data.data[i][j]);
+        }
+        fprintf(stdout, "|\n");
+    }
+    fprintf(stdout, "]\n");
+}
+
+void 
+print_mat(const MATRICES matrices) {
+    printf("p");
+    MAT *mat;
+    int i, j;
     char temp_mat[10];
-    scanf("%s", &temp_mat);
+
+    scanf("%s", temp_mat);
+
     mat = sort_mat(temp_mat, matrices);
-    for (int i = 0; i < LEN_OF_MAT; i++) {
-        for (int j = 0; j < LEN_OF_MAT; j++) {
-            printf("%2.2f\t", mat->values[i][j]);
+
+    for (i = 0; i < LEN_OF_MAT; i++) {
+        for (j = 0; j < LEN_OF_MAT; j++) {
+            printf("%-2.2f\t", mat->data[i][j]);
         }
         printf("\n");
     }
 }
 
-MATRICES *sort_mat(const char mat_name, MATRICES matrices) {
+MAT
+*sort_mat(const char *mat_name, MATRICES matrices) {
+    printf("S");
     if (strcmp(mat_name, "mat_a") == 0) {
         return matrices.MAT_A;
     } else if (strcmp(mat_name, "mat_b") == 0) {
@@ -42,18 +57,16 @@ MATRICES *sort_mat(const char mat_name, MATRICES matrices) {
     } else if (strcmp(mat_name, "mat_f") == 0) {
         return matrices.MAT_F;
     } else {
-        // Return NULL or handle the case for an unknown matrix name
         return NULL;
     }
 }
-
-
 
 int mymat(MATRICES mats) {
     char command[20];
 
     while (scanf("%s", command) != EOF) {
         if (strcmp(command, "print_mat") == 0) {
+            printf("V");
             print_mat(mats);
         }
         else if (strcmp(command, "stop") == 0) {
@@ -69,10 +82,10 @@ int mymat(MATRICES mats) {
 
 int main(void) {
     MAT MAT_A;
-    MATRICES *my_mats;
-    my_mats->MAT_A = &MAT_A;
+    MATRICES my_mats;
+    my_mats.MAT_A = &MAT_A;
 
-    reset_mat(my_mats->MAT_A);
+    reset_mat(my_mats.MAT_A[0]);
     mymat(my_mats);
 
     return 0;
---
+++ mamans/maman-13/mymat.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LEN_OF_MAT 4
+#define PPRINT_MAT(MAT) pprint_mat(#MAT, (MAT))
+
+typedef struct {
+    float data[LEN_OF_MAT][LEN_OF_MAT];
+} MAT;
+
+typedef struct {
+    MAT *MAT_A;
+    MAT *MAT_B;
+    MAT *MAT_C;
+    MAT *MAT_D;
+    MAT *MAT_E;
+    MAT *MAT_F;
+}MATRICES;
+
+extern int 
+mymat(MATRICES mats);
+extern MAT 
+*sort_mat(const char *mat_name, MATRICES matrices);
+extern void 
+reset_mat(MAT mat);
+extern void 
+print_mat(const MATRICES matrices);
+extern void 
+pprint_mat(const char *matrix_name, const MAT matrix_data);
+
