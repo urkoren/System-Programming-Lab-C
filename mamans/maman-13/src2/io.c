52238927f45dfb2c19fc514019ac0fb897ae1ed1
SagiKimhi <sagi.kim5@gmail.com>
2024-02-09 07:40:45 -0500

    ci

--- mamans/maman-13/src2/io.c
+++ mamans/maman-13/src2/io.c
@@ -1,29 +1,86 @@
 #include "io.h"
 
 
+int
+is_in_tok_buf(const char c, const char *tok_buf)
+{
+    int i = 0;
+
+    IO_BUF_ASSERT( tok_buf )
+    
+    while ( tok_buf[i] ) {
+        if ( is_tok(c, tok_buf[i]) )
+            return 1;
+        i++;
+    }
+    return 0;
+}
+
 int 
-skip_to_token(FILE *f, char token, bool_t ignore_eol)
+fskip_to_any(FILE *f, const char *tokens, bool_t ignore_eol)
+{
+}
+
+int 
+fskip_to_token(FILE *f, char tok, bool_t ignore_eol)
+{
+    char c = 0;
+    int done = 0;
+
+    IO_F_ASSERT(f)
+
+    while (!done) {
+        c = fgetc(f);
+        done = (
+            feof(f) 
+            || ( ignore_eol && is_tok(c, tok) ) 
+            || is_eol_or_tok(c, tok) 
+        );
+    }
+    
+    return ( is_tok(c, tok) ? ungetc(c, f) : EOF );
+}
+
+int
+fskip_whitespace(FILE *f)
 {
     char c;
-    assert(f);
-    for (c = fgetc(f); !is_eol_or_tok(c, token); c = fgetc(f)) {}
-    return (feof(f) ? EOF: ungetc(c, f));
+    IO_F_ASSERT(f)
+    while ( !feof(f) && !isgraph(c = fgetc(f)) ) {}
+    return ( isgraph(c) ? ungetc(c, f) : EOF );
 }
 
 int
-skip_spaces(FILE *f)
+fskip_graphic(FILE *f)
 {
-    return 0;
+    char c;
+    IO_F_ASSERT(f)
+    while ( !feof(f) && isgraph(c = fgetc(f)) ) {}
+    return ( (!isgraph(c)) ? ungetc(c, f) : EOF );
 }
 
 int
-get_word(FILE *f, char *buf, size_t maxlen)
+fget_word(FILE *f, char *buf, size_t maxlen)
 {
-    return 0;
+    char c = 0;
+    size_t i = 0;
+    
+    IO_F_ASSERT(f)
+    IO_BUF_ASSERT(buf)
+
+    if ( !isgraph(c = fgetc(f)) && !feof(f) )
+        fskip_whitespace(f); 
+
+    while ( isgraph(c = fgetc(f)) && i < (maxlen - 1) )
+        buf[i++] = c;
+
+    buf[i] = 0;
+
+    return ( feof(f) ? EOF : ungetc(c, f) );
 }
 
 int
-get_line(FILE *f, char *dst, size_t maxlen)
+fget_line(FILE *f, char *dst, size_t maxlen)
 {
     return 0;
 }
--- mamans/maman-13/src2/io.h
+++ mamans/maman-13/src2/io.h
@@ -7,32 +7,50 @@
 
 /* Defines */
 
+#ifndef IO_F_ASSERT
+#   define IO_F_ASSERT(f) if ( (f) == NULL || feof(f) ) { return EOF; }
+#endif
+
+#ifndef IO_BUF_ASSERT
+#   define IO_BUF_ASSERT(buf) if ( (buf) == NULL ) { return -1; }
+#endif
+
 #ifndef is_eol
-#   define is_eol(c) ( (c) == '\n' || (c) == EOF )
+#   define is_eol(c) ( (int)((c) == '\n' || (c) == EOF) )
 #endif 
 
 #ifndef is_tok
-#   define is_tok(c, tok) ( (c) == (tok) )
+#   define is_tok(c, tok) ( (int)((c) == (tok)) )
 #endif
 
 #ifndef is_eol_or_tok
-#   define is_eol_or_tok(c, tok) ( is_eol(c) || is_tok((c), (tok)) )
+#   define is_eol_or_tok(c, tok) ( (int)(is_eol(c) || is_tok((c), (tok))) )
 #endif
+
+extern int
+is_in_tok_buf(const char c, const char *tok_buf);
 
 /* Skip Methods */
 
 extern int 
-skip_to_token(FILE *f, char token, bool_t ignore_eol);
+fskip_to_any(FILE *f, const char *tokens, bool_t ignore_eol);
+
+extern int 
+fskip_to_token(FILE *f, char token, bool_t ignore_eol);
 
 extern int
-skip_spaces(FILE *f);
+fskip_whitespace(FILE *f);
+
+extern int
+fskip_graphic(FILE *f);
+
 
 /* Get Methods */
 
 extern int
-get_word(FILE *f, char *dst, size_t maxlen);
+fget_word(FILE *f, char *dst, size_t maxlen);
 
 extern int
-get_line(FILE *f, char *dst, size_t maxlen);
+fget_line(FILE *f, char *dst, size_t maxlen);
 
 #endif /* IO_H_ */
